5b93daab2589cfb7d33d83df1b999a8de5352330
JAuxance <auxance.jourdan@proton.me>
2025-12-18 13:51:25 +0100

    refactor user_command.c for improved input handling and remove string_to_arr.c

--- concept_exo/user_command.c
+++ concept_exo/user_command.c
@@ -4,12 +4,26 @@
  *
  * Return: 0 on success
  */
+
 int main(void)
 {
-	size_t n = 10;
+	size_t n = 0;
 	char *buffer = NULL;
+	ssize_t read;
 
-	printf("$ - ");
-	getline(&buffer, &n, stdin);
+	while (1)
+	{
+		write(1, "$ - ", 4);
+		read = getline(&buffer, &n, stdin);
+		if (read == -1)
+		{
+			printf("Error reading line\n");
+			free(buffer);
+			return (1);
+		}
+		write(1, buffer, read);
+	}
+
+	free(buffer);
 	return (0);
 }
