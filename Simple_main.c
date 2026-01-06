fdd56070627d9bca9856e9877c138f506cf5c8e6
JAuxance <auxance.jourdan@proton.me>
2026-01-06 08:35:07 +0100

    Refactor code structure for improved readability and maintainability

--- Simple_main.c
+++ Simple_main.c
@@ -17,20 +17,23 @@ int main(void)
 	while (1)
 	{
 		r = read_and_parse(&buffer, &n, args, is_interactive);
-
 		if (r == -1) /* EOF (Ctrl+D)*/
 		{
 			free(buffer);
 			return (last_exit_code);
 		}
-
 		/*If you decide read_and_pars can return 0 on error*/
 		if (r == 0)
 			continue;
 		/*Skip empty lines*/
-
 		if (args[0] == NULL)
 			continue;
+
+		if (strcmp(args[0], "exit") == 0)
+		{
+			free(buffer);
+			exit(last_exit_code);
+		}
 
 		cmd_path = find_command_in_path(args[0]);
 		if (cmd_path != NULL)
@@ -40,9 +43,7 @@ int main(void)
 			free(cmd_path);
 		}
 		else
-		{
 			fprintf(stderr, "./hsh: 1: %s: not found\n", args[0]);
-			last_exit_code = 127;
-		}
+		last_exit_code = 127;
 	}
 }
