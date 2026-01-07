d0c757ae31ff0dae75362d758f75cf1f62cc00e5
jhinlang <julienhinlang@gmail.com>
2026-01-07 14:16:04 +0100

    Fix return value in execute_command documentation
    Updated return value in documentation and improved comments.

--- execute_command.c
+++ execute_command.c
@@ -3,35 +3,35 @@
  * execute_command - executes a command using fork and execve
  * @args: array of arguments for the command
  * @envp: environment variables
- * Return: exit status of command or -1 on error
+ * Return: exit status of command or 1 on error
  */
 
 int execute_command(char *args[], char **envp)
 {
-	pid_t pid;
-	int wstatus;
+	pid_t pid; 		/* process identifier (fork return value) */
+	int wstatus;    /* filled by waitpid, contain exit code or signal */
 
-	pid = fork();
-	if (pid < 0)
+	pid = fork(); 	/* duplicate the processus */
+	if (pid < 0)  	/* pid < 0 error, fork dont work */
 	{
 		perror("fork");
 		return (1);
 	}
 
-	if (pid == 0)
+	if (pid == 0) /* we are in the child, we replace it */
 	{
-		execve(args[0], args, envp);
-		perror(args[0]);
-		_exit(127);
+		execve(args[0], args, envp);  /* args[0] = /bin/ls, args = array of the command /bin/ls -l /tmp */
+		perror(args[0]);			  /*args[0] = /bin/ls */
+		_exit(127);					 
 	}
 
-	if (waitpid(pid, &wstatus, 0) < 0)
+	if (waitpid(pid, &wstatus, 0) < 0) /* parent wait pid end and how it end, exit code, signal */
 	{
-		perror("waitpid");
+		perror("waitpid");			   /* print error message "waitpid: No child processes" */
 		return (1);
 	}
 
-	if (WIFEXITED(wstatus))
+	if (WIFEXITED(wstatus))			   /* if child process ended normally, return the same exit code */
 		return (WEXITSTATUS(wstatus));
 
 	return (1);
